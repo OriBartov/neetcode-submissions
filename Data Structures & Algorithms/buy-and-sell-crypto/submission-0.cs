096e07be0b09299ad972796610cb1295e272df06
Ori Bartov <37376910+OriBartov@users.noreply.github.com>
2026-05-27 12:24:52 +0300

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cs
@@ -0,0 +1,23 @@
+
+
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        int maxProfit = 0;
+        int l = 0, r = 1;
+
+        while (r < prices.Length)
+        {
+            if (prices[l] < prices[r])
+            {
+                maxProfit = System.Math.Max(maxProfit, prices[r] - prices[l]);
+            }
+            else
+            {
+                l = r;
+            }
+            r++;
+        } 
+
+        return maxProfit;
+    }
+}
